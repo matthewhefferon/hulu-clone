9220823a0c58296bc0b190a0ee5602c865bd9728
Matthew Hefferon <matthewhefferon@Matthews-MacBook-Air.local>
2020-12-11 09:58:42 -0700

    final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Nav from './components/Nav';
+import Results from './components/Results';
+import requests from "./requests"
+
 
 function App() {
+const [selectedOption, setSelectedOption] = useState(requests.fetchTrending);
+
   return (
     <div className="app">
       <Header />
-      <Nav />
+      <Nav setSelectedOption={setSelectedOption}/>
+      <Results selectedOption ={selectedOption}  />
     </div>
   );
 }
